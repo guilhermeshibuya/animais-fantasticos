22a0f1d99cbbee17aafd735e634b85ae5b45d128
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-01-06 15:00:08 -0300

    add: smooth scroll

--- script.js
+++ script.js
@@ -39,3 +39,31 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initSmoothScroll() {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // Forma alternativa
+    // const top = section.offsetTop;
+
+    // window.scrollTo({
+    //   top: top,
+    //   behavior: "smooth",
+    // });
+  }
+
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initSmoothScroll();
