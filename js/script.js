53cdb51af5e84560c4dbf1956c9744e8e72f1631
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-01-11 15:53:01 -0300

    add: tooltip

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ import initScrollAnimation from "./modules/scroll-animation.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabNav();
 initAccordion();
 initSmoothScroll();
 initScrollAnimation();
 initModal();
+initTooltip();
 
 // Adicione um atributo data-anime="show-down" e
 // data-anime="show-right" a todos as section's
