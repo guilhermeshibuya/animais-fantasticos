8d3627f0223adbec7f788b394938d3454415412c
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-01-25 22:16:46 -0300

    menu mobile refatorado

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,23 +1,37 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const events = ["click", "touchstart"];
+export default class MenuMobile {
+  constructor(menuButton, menuList, events) {
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menuList);
+    this.activeClass = "active";
 
-  function openMenu() {
-    menuList.classList.add("active");
-    menuButton.classList.add("active");
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
 
-    outsideClick(menuList, events, () => {
-      menuList.classList.remove("active");
-      menuButton.classList.remove("active");
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.menuList.classList.add(this.activeClass);
+    this.menuButton.classList.add(this.activeClass);
+
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.activeClass);
+      this.menuButton.classList.remove(this.activeClass);
     });
   }
 
-  if (menuButton) {
-    events.forEach((userEvent) =>
-      menuButton.addEventListener(userEvent, openMenu)
+  addMenuMobileEvents() {
+    this.events.forEach((userEvent) =>
+      this.menuButton.addEventListener(userEvent, this.openMenu)
     );
+  }
+
+  init() {
+    if (this.menuButton && this.menuList) {
+      this.addMenuMobileEvents();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import TabNav from "./modules/tabnav.js";
 import Tooltip from "./modules/tooltip.js";
 import ScrollAnimation from "./modules/scroll-animation.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBtc from "./modules/fetch-btc.js";
@@ -37,10 +37,12 @@ fetchAnimais("../api/animais.json", ".numeros-grid");
 const scrollAnimation = new ScrollAnimation('[data-anime="scroll"]');
 scrollAnimation.init();
 
-const dropdownMenu = new DropdownMenu();
-dropdownMenu.init("[data-dropdown]", ["touchstart", "click"]);
+const dropdownMenu = new DropdownMenu("[data-dropdown]");
+dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initFuncionamento();
 
 fetchBtc("https://blockchain.info/ticker", ".btc-preco");
